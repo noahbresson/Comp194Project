131f999df9b179a2f670a1e976a6a3d13b4921ab
JamesMroz <jmroz@macalester.edu>
2020-10-16 10:06:26 -0500

    added list

--- Assets/Scripts/BinDrop.cs
+++ Assets/Scripts/BinDrop.cs
@@ -4,9 +4,26 @@ using UnityEngine;
 
 public class BinDrop : MonoBehaviour
 {
+     public list<String> trashList;
+     public list<String> compostList;
+     public list<String> recycleList;
     // Start is called before the first frame update
     void Start()
     {
+        trashList = new list<String>(); 
+        trash.Add("ChipBag");
+        trash.Add("Wrapper");
+
+        compostList = new list<String>();
+        compost.Add("Banana");
+        compost.Add("Fork");
+        compost.Add("Apple");
+        compost.Add("Orange");
+        
+        recycleList = new list<String>();
+        recycle.Add("Paper");
+        recycle.Add("Cardboard");
+        recycle.Add("Bottle");
         
     }
 
